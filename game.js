3c3a9765d9d916f8848ca95f957cb5839ada7c8d
GouthamA15 <lucky.masna1@gmail.com>
2024-06-11 23:15:19 +0530

    Add Game

---
+++ game.js
@@ -0,0 +1,24 @@
+// Guess the Number
+
+let gameNum = 8;
+let Num = prompt("Guess the Number That I've stored ");
+let chance = 5;
+
+
+while (Num != gameNum && chance > 0) {
+    Num = prompt("Your Wrong, Guess the Number Again. You have few chances");
+    chance--;
+    if (chance == 0) {
+        break;
+    }
+    console.log("You have",chance,"chances left");
+}
+
+
+if(Num == gameNum) {
+    alert("Congratulations, You've Won");
+    console.log("Refresh to Start Again");
+}
+else {
+    alert("Sorry, You've Lost");
+}
